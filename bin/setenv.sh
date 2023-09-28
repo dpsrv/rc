5b64be01f1a12a3793f6036818ab9feaf09511aa
Maxim Fortun <max@maxf.net>
2023-09-27 20:25:45 -0400

    updated

--- bin/setenv.sh
+++
@@ -1,9 +0,0 @@
-cd $(dirname ${BASH_SOURCE[0]})/../..
-export DPSRV_HOME=$PWD
-cd $OLDPWD
-
-export DPSRV_SERVICES=( $( grep -l 'restart:[ ]*unless-stopped' $DPSRV_HOME/*/docker-compose.yml | sed "s#^$DPSRV_HOME/##g"|cut -d/ -f1 ) )
-
-if ! [[ "$PATH" =~ "$DPSRV_HOME/rc/bin" ]]; then
-	export PATH="$PATH:$DPSRV_HOME/rc/bin"
-fi
