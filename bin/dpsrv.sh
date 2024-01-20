4dde4001d29444007b368011e57ae0730d7bb2e5
Max <max@maxf.net>
2024-01-20 17:46:23 -0500

    updated

--- bin/dpsrv.sh
+++ bin/dpsrv.sh
@@ -195,14 +195,14 @@ function dpsrv-iptables-forward-port() {(
 		[ -n "$dstAddr" ] || continue
 		[ -n "$toAddr" ] || continue
 
-		local dnat="-t nat ! -i $brideIF -p $proto --dport $dport -j DNAT --to-destination $toAddr:$dport -m comment --comment $comment"
+		local dnat="-t nat -p $proto --dport $dport -j DNAT --to-destination $toAddr:$dport -m comment --comment $comment"
 		#local redirect="-t nat -p $proto --dport $dport -j REDIRECT --to-port $cport -m comment --comment $comment"
 
 		# Accept connections on port $dport
 		sudo /sbin/${iptables} $accept
 
 		# DNAT external traffic
-		sudo /sbin/${iptables} -I PREROUTING -d $dstAddr $dnat
+		sudo /sbin/${iptables} -I PREROUTING ! -i $brideIF -d $dstAddr $dnat
 
 		# DNAT internal traffic
 		sudo /sbin/${iptables} -I OUTPUT -d $localAddr,$dstAddr $dnat
