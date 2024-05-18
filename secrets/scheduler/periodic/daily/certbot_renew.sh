831945889b85f13f8f3daa01ebfc8568173cbfff
Maxim Fortun <max@maxf.net>
2024-05-18 02:21:00 +0000

    updated

--- secrets/scheduler/periodic/daily/certbot_renew.sh
+++ secrets/scheduler/periodic/daily/certbot_renew.sh
@@ -1,2 +1,2 @@
-b5k4B+10cEviaYgoJYY70vpSV9QIrpN+TPvn4YKaImlCleeXut1NpQPS1GtAUaFX
-kjDFMzOr8RaK5HUVzMQRsw==
+b5k4B+10cEviaYgoJYY70vpSV9QIrpN+TPvn4YKaImmOgYHeBmDxrNZgHxh2wf36
+YrrsrPF+bfVgBfs7uku3jQ==
