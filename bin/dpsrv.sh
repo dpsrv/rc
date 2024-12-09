20d3c9e5b53a36357a6f9edd19dcfd1c8317e7e4
Maxim Fortun <max@maxf.net>
2024-12-09 19:12:04 +0000

    updated

--- bin/dpsrv.sh
+++ bin/dpsrv.sh
@@ -405,19 +405,19 @@ function dpsrv-logs-truncate() {(
 	# Get the disk usage of the logs in kilobytes and sort them by size
 	local logs=$(du -k /var/lib/docker/containers/**/*-json.log | sort -hr)
 
-# Loop through each line of the output
-while IFS= read -r line; do
-  # Extract the size and the file path
-  size=$(echo $line | awk '{print $1}')
-  filepath=$(echo $line | awk '{print $2}')
+	# Loop through each line of the output
+	while IFS= read -r line; do
+		# Extract the size and the file path
+		local size=$(echo $line | awk '{print $1}')
+		local filepath=$(echo $line | awk '{print $2}')
 
-  # Check if the size is more than 100MB (100MB = 102400KB)
-  if [ "$size" -gt 102400 ]; then
-    echo "Truncating $filepath (Size: $size KB)"
-    # Truncate the log file
-    > "$filepath"
-  fi
-done <<< "$logs"
+		# Check if the size is more than 100MB (100MB = 102400KB)
+		if [ "$size" -gt 102400 ]; then
+			echo "Truncating $filepath (Size: $size KB)"
+			# Truncate the log file
+			> "$filepath"
+		fi
+	done <<< "$logs"
 
-echo "Log truncation complete."
+	echo "Log truncation complete."
 )}
