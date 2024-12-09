03927d7cd8ca05cd04823120f3ba0bc457087bb0
Maxim Fortun <max@maxf.net>
2024-12-09 19:15:00 +0000

    updated

--- bin/dpsrv.sh
+++ bin/dpsrv.sh
@@ -401,7 +401,7 @@ function dpsrv-k8s-secrets() {(
 
 )}
 
-function dpsrv-logs-truncate() {(
+function dpsrv-logs-truncate() {sudo (
 	set -x
 	# Get the disk usage of the logs in kilobytes and sort them by size
 	local logs=$(du -k /var/lib/docker/containers/**/*-json.log | sort -hr)
