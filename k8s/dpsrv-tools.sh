06ae24db76b4743904578e71ce03ce430f94a4a8
rc <rc@dpsrv.me>
2025-08-17 16:46:13 +0000

    updated

--- k8s/dpsrv-tools.sh
+++ k8s/dpsrv-tools.sh
@@ -1,3 +1,10 @@
 #!/bin/bash -ex
 
-kubectl -n dpsrv run -it --rm --restart=Never dpsrv-tools --overrides='{ "spec": { "serviceAccountName": "dpsrv-admin" } }' --image=maxfortun/private:alpine-tools-2 -- sh
+kubectl -n dpsrv \
+	run dpsrv-tools \
+	--image=maxfortun/private:alpine-tools-2 \
+	-it --rm --restart=Never \
+	--overrides='{
+		"spec": { "serviceAccountName": "dpsrv-admin" }
+	}' \
+	-- sh
