3fbb566ac140a6b6cddd1d26a8b1856c2d6b92e7
rc <rc@dpsrv.me>
2025-08-19 01:39:15 +0000

    updated

--- docker/files/usr/local/bin/rc-refresh.sh
+++ docker/files/usr/local/bin/rc-refresh.sh
@@ -27,7 +27,7 @@ for secrets in $EXPORT_SECRETS; do
 	find $dir ! -type d | while read file; do
 		secret=${file#/mnt/data/dpsrv/rc/secrets/}
 		secret=${secret//\//-}
-		secret=${secret,,}
+		secret=$(echo $secret|tr A-Z a-z)
 		kubectl -n $ns create secret generic $secret --from-file=$file \
 			--dry-run=client -o yaml | kubectl apply -f - | grep -v unchanged
 	done
