b205cfda2eeb88d6c8fa30ffe424e60dcc4c4800
rc <rc@dpsrv.me>
2025-08-23 17:33:15 +0000

    updated

--- docker/files/usr/local/bin/export-files.sh
+++ docker/files/usr/local/bin/export-files.sh
@@ -6,11 +6,8 @@ SECRET_FILES=$(cat <<_EOT_
 _EOT_
 )
 
-echo $SECRET_FILES | while read secret_files_rule; do
-	echo $secret_files_rule
+echo "$SECRET_FILES" | while read secret_files_rule; do
 	read -r secret_files_ns secret_files_path secret_files_xform <<< "${secret_files_rule}"
-	echo "1: $secret_files_ns 2:$secret_files_path 3:$secret_files_xform"
-	continue
         find $SECRET_FILES_DIR/$secret_files_path ! -type d | while read file; do
 		secret_path=$(echo $file | sed "s#$SECRET_FILES_DIR/*##g")
 		secret_name=$(echo $secret_path| sed $secret_files_xform | sed 's#/#-#g' | tr A-Z a-z)
