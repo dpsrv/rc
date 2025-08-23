c3b1cb969a62aaea67e648a37de69db72655fd03
rc <rc@dpsrv.me>
2025-08-23 19:44:18 +0000

    updated

--- docker/files/usr/local/bin/export-env.sh
+++ docker/files/usr/local/bin/export-env.sh
@@ -10,16 +10,12 @@ echo "$SECRET_ENV" | while read secret_env_rule; do
 	secret_env_path=$SECRET_ENV_DIR/$secret_env_file
 	cat $secret_env_path | while read secret_env; do
 		read -r secret_name secret_value <<< "${secret_env/=/ }"
-		echo "${secret_name} __ $secret_value"
-	done
-	exit
-
-	secret_name=$secret_path
-	[ -z "$secret_files_xform" ] || secret_name=$(echo $secret_name | sed $secret_files_xform)
+		[ -z "$secret_env_xform" ] || secret_name=$(echo $secret_name | sed $secret_env_xform)
 	secret_name=$(echo $secret_name | sed 's#/#-#g' | tr A-Z a-z)
 	exit
                 kubectl -n $secret_files_ns create secret generic $secret_name --from-file=$file \
                         --dry-run=client -o yaml | kubectl apply -f - | grep -v unchanged
 		exit 
+	done
 done
 
