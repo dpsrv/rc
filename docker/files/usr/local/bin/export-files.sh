7ae728bb89660bbddca457cfb0611ab8bcad482b
rc <rc@dpsrv.me>
2025-08-23 17:18:36 +0000

    updated

--- docker/files/usr/local/bin/export-files.sh
+++ docker/files/usr/local/bin/export-files.sh
@@ -11,13 +11,8 @@ for secret_files_rule in $SECRET_FILES; do
 
         find $SECRET_FILES_DIR/$secret_files_path ! -type d | while read file; do
 		secret_path=$(echo $file | sed "s#$SECRET_FILES_DIR/*##g")
-		secret_name=$(echo $secret_path| sed $secret_files_xform | sed 's#/#-#g')
-		echo "$file -> $secret_name"
-		continue
-                secret=${file#/mnt/data/dpsrv/rc/secrets/}
-                secret=${secret//\//-}
-                secret=$(echo $secret|tr A-Z a-z)
-                kubectl -n $ns create secret generic $secret --from-file=$file \
+		secret_name=$(echo $secret_path| sed $secret_files_xform | sed 's#/#-#g' | tr A-Z a-z)
+                echo kubectl -n $ns create secret generic $secret --from-file=$file \
                         --dry-run=client -o yaml | kubectl apply -f - | grep -v unchanged
         done
 done
