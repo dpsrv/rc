e551c67f651c0c3fe07d061b45384f1ac31463a0
rc <rc@dpsrv.me>
2025-08-23 19:36:26 +0000

    updated

--- docker/files/usr/local/bin/export-env.sh
+++ docker/files/usr/local/bin/export-env.sh
@@ -9,7 +9,8 @@ echo "$SECRET_ENV" | while read secret_env_rule; do
 	read -r secret_env_ns secret_env_file secret_env_xform <<< "${secret_env_rule}"
 	secret_env_path=$SECRET_ENV_DIR/$secret_env_file
 	(
-		env -i sh --noprofile --norc ". $secret_env_path"
+		#env -i sh --noprofile --norc ". $secret_env_path"
+		sh --noprofile --norc ls
 	)
 	exit
 
