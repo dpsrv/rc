d0371c6cc7107292fe5f1eb7a795a1b251b50d61
rc <rc@dpsrv.me>
2025-08-23 18:12:23 +0000

    updated

--- docker/files/usr/local/bin/export-files.sh
+++ docker/files/usr/local/bin/export-files.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 
-SECRET_FILES=$(cat <<_EOT_
-    dpsrv dpsrv/rc/secrets/letsencrypt/live/domain/ s#^dpsrv/rc/secrets/##g
-    dpsrv dpsrv/rc/secrets/mongo/conf/ s#^dpsrv/rc/secrets/##g
-_EOT_
-)
-
 echo "$SECRET_FILES" | while read secret_files_rule; do
 	read -r secret_files_ns secret_files_path secret_files_xform <<< "${secret_files_rule}"
         find $SECRET_FILES_DIR/$secret_files_path ! -type d | while read file; do
