473b88a02bf9e7c60cac3b61ffe5615fb766e016
rc <rc@dpsrv.me>
2025-08-24 03:03:21 +0000

    updated

--- docker/files/usr/local/bin/export-env.sh
+++ docker/files/usr/local/bin/export-env.sh
@@ -4,14 +4,18 @@
 
 echo "$SECRET_ENV" | while read secret_env_rule; do
 
-	secret_env_rule_file=/tmp/$(basename $0).secret_env_rule.$$
+	secret_env_rule_file=/tmp/$(basename $0).$$.secret_env_rule
 	echo "$secret_env_rule" > $secret_env_rule_file
 	read -r secret_env_ns secret_env_file secret_env_xform < $secret_env_rule_file
 	rm $secret_env_rule_file
 
 	secret_env_path=$SECRET_ENV_DIR/$secret_env_file
 	cat $secret_env_path | while read secret_env; do
-		read -r secret_name secret_value <<< "${secret_env/=/ }"
+		secret_env_file=/tmp/$(basename $0).$$.secret_env
+		echo "${secret_env/=/ }" > $secret_env_file
+		read -r secret_name secret_value < $secret_env_file
+		rm $secret_env_file
+
 		[ -z "$secret_env_xform" ] || secret_name=$(echo $secret_name | sed $secret_env_xform)
 		secret_name=$(echo $secret_name | tr A-Z_ a-z-)
 
