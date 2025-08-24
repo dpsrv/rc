ef6076cbe5042dd11cd7bef4d5a580cf92b0e70d
rc <rc@dpsrv.me>
2025-08-24 02:58:40 +0000

    updated

--- docker/files/usr/local/bin/export-files.sh
+++ docker/files/usr/local/bin/export-files.sh
@@ -8,11 +8,11 @@ echo "$SECRET_FILES" | while read secret_files_rule; do
 	read -r secret_files_ns secret_files_path secret_files_xform < $secret_files_rule_file
 	rm $secret_files_rule_file
 
-        find $SECRET_FILES_DIR/$secret_files_path ! -type d | while read file; do
+	find $SECRET_FILES_DIR/$secret_files_path ! -type d | while read file; do
 		secret_path=$(echo $file | sed "s#$SECRET_FILES_DIR/*##g")
 		secret_name=$(echo $secret_path| sed $secret_files_xform | sed 's#/#-#g' | tr A-Z a-z)
-                kubectl -n $secret_files_ns create secret generic $secret_name --from-file=$file \
-                        --dry-run=client -o yaml | kubectl apply -f - | grep -v unchanged
-        done
+		kubectl -n $secret_files_ns create secret generic $secret_name --from-file=$file \
+			--dry-run=client -o yaml | kubectl apply -f - | grep -v unchanged
+	done
 done
 
