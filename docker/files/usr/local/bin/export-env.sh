a54ae32251663954918c89ef559190627d4fb4dc
rc <rc@dpsrv.me>
2025-09-06 02:26:08 +0000

    updated

--- docker/files/usr/local/bin/export-env.sh
+++ docker/files/usr/local/bin/export-env.sh
@@ -8,6 +8,7 @@ echo "$SECRET_ENV" | while read secret_env_rule; do
 	echo "$secret_env_rule" > $secret_env_rule_file
 	read -r secret_env_ns secret_env_file secret_env_xform < $secret_env_rule_file
 	rm $secret_env_rule_file
+	[ -n "$secret_env_file" ] || continue
 
 	secret_env_path=$SECRET_ENV_DIR/$secret_env_file
 	cat $secret_env_path | while read secret_env; do
