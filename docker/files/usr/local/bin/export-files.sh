b0774b90484259530278f31b8c387ce9b5a2d6e7
rc <rc@dpsrv.me>
2025-09-18 18:50:33 +0000

    updated

--- docker/files/usr/local/bin/export-files.sh
+++ docker/files/usr/local/bin/export-files.sh
@@ -10,7 +10,7 @@ echo "$SECRET_FILES" | while read secret_files_rule; do
 	read -r secret_files_ns secret_files_path secret_files_xform < $secret_files_rule_file
 	rm $secret_files_rule_file
 
-	find "$SECRET_FILES_DIR/$secret_files_path" ! -type d | while read file; do
+	find $SECRET_FILES_DIR/$secret_files_path ! -type d | while read file; do
 		[ -e "$file" ] || continue
 		if [[ "$file" =~ '\.envsubst$' ]]; then
 			rendered=${file%.envsubst}
